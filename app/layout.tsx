709421c72541e81f0b004ef4493f2aca5d2dde84
kashafnaz24 <kashafnazk389@gmail.com>
2024-11-10 23:41:13 +0500

    portfolio website through custum css

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar/page";
 import Footer from "./Footer/page";
 import "./globals.css";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function RootLayout({children}:any){
   return(
     <html lang="en">
